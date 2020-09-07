2eaab3e36729dee53293ff425c19232a81f019f9
Nikhil Joseph Peter <josef.nikhil18@gmail.com>
2020-09-07 12:40:09 +0530

    Eager vs lazy execute

--- src/com/nick/hibernate/demo/EagerLazyDemo.java
+++ src/com/nick/hibernate/demo/EagerLazyDemo.java
@@ -46,3 +46,20 @@ public class EagerLazyDemo {
 	}
 
 }
+
+//EAGER
+/*
+ * Hibernate: select instructor0_.id as id1_1_0_, instructor0_.email as email2_1_0_, instructor0_.first_name as first_na3_1_0_, instructor0_.instructor_detail_id as instruct5_1_0_, instructor0_.last_name as last_nam4_1_0_, courses1_.instructor_id as instruct3_0_1_, courses1_.id as id1_0_1_, courses1_.id as id1_0_2_, courses1_.instructor_id as instruct3_0_2_, courses1_.title as title2_0_2_, instructor2_.id as id1_2_3_, instructor2_.hobby as hobby2_2_3_, instructor2_.youtube_channel as youtube_3_2_3_ from instructor instructor0_ left outer join course courses1_ on instructor0_.id=courses1_.instructor_id left outer join instructor_detail instructor2_ on instructor0_.instructor_detail_id=instructor2_.id where instructor0_.id=?
+ * Instructor(id=1, firstName=nick, lastName=pete, email=pete@gmail.com, instructorDetial=InstructorDetail [youtubeChannel=http://www.pete.com, hobby=sleeping], courses=[Course [id=12, title=coding], Course [id=13, title=Pinball]])
+ * InstructorDetail [youtubeChannel=http://www.pete.com, hobby=sleeping]
+ * [Course [id=12, title=coding], Course [id=13, title=Pinball]]
+ */
+
+//Lazy
+/*
+ * Hibernate: select instructor0_.id as id1_1_0_, instructor0_.email as email2_1_0_, instructor0_.first_name as first_na3_1_0_, instructor0_.instructor_detail_id as instruct5_1_0_, instructor0_.last_name as last_nam4_1_0_, instructor1_.id as id1_2_1_, instructor1_.hobby as hobby2_2_1_, instructor1_.youtube_channel as youtube_3_2_1_ from instructor instructor0_ left outer join instructor_detail instructor1_ on instructor0_.instructor_detail_id=instructor1_.id where instructor0_.id=?
+ * Hibernate: select courses0_.instructor_id as instruct3_0_0_, courses0_.id as id1_0_0_, courses0_.id as id1_0_1_, courses0_.instructor_id as instruct3_0_1_, courses0_.title as title2_0_1_ from course courses0_ where courses0_.instructor_id=?
+ * Instructor(id=1, firstName=nick, lastName=pete, email=pete@gmail.com, instructorDetial=InstructorDetail [youtubeChannel=http://www.pete.com, hobby=sleeping], courses=[Course [id=12, title=coding], Course [id=13, title=Pinball]])
+ * InstructorDetail [youtubeChannel=http://www.pete.com, hobby=sleeping]
+ * [Course [id=12, title=coding], Course [id=13, title=Pinball]]
+ */
