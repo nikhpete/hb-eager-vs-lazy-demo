285e0d957ef10cfdc68c43bdb8120d53c328aa4f
Nikhil Joseph Peter <josef.nikhil18@gmail.com>
2020-09-07 17:00:47 +0530

    option 2 lazy fetch with HQL

---
+++ src/com/nick/hibernate/demo/FetchJoinDemo.java
@@ -0,0 +1,85 @@
+package com.nick.hibernate.demo;
+
+import java.text.ParseException;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import com.nick.hibernate.demo.entity.Course;
+import com.nick.hibernate.demo.entity.Instructor;
+import com.nick.hibernate.demo.entity.InstructorDetail;
+
+public class FetchJoinDemo {
+
+	public static void main(String[] args) throws ParseException {
+
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			session.beginTransaction();
+
+			//			Query<Instructor> query = session.createQuery("SELECT i from Instructor i "
+			//														+"JOIN FETCH i.courses "
+			//														+"where i.id=:insId",Instructor.class);
+
+			Query<Course> query = session.createQuery("select c from Course c "
+					+ "where c.instructor.id=:insId",    
+					Course.class);
+			
+			int id = 1;
+
+			query.setParameter("insId", id);
+
+			Instructor ins = session.get(Instructor.class, id);
+
+			System.out.println(ins);
+
+			System.out.println(ins.getInstructorDetial());
+
+			List<Course> course = query.getResultList();
+
+			session.getTransaction().commit();
+
+			session.close();
+
+			System.out.println("Session closed!!!");
+
+			System.out.println(course);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
+
+//EAGER
+/*
+ * Hibernate: select instructor0_.id as id1_1_0_, instructor0_.email as email2_1_0_, instructor0_.first_name as first_na3_1_0_, instructor0_.instructor_detail_id as instruct5_1_0_, instructor0_.last_name as last_nam4_1_0_, courses1_.instructor_id as instruct3_0_1_, courses1_.id as id1_0_1_, courses1_.id as id1_0_2_, courses1_.instructor_id as instruct3_0_2_, courses1_.title as title2_0_2_, instructor2_.id as id1_2_3_, instructor2_.hobby as hobby2_2_3_, instructor2_.youtube_channel as youtube_3_2_3_ from instructor instructor0_ left outer join course courses1_ on instructor0_.id=courses1_.instructor_id left outer join instructor_detail instructor2_ on instructor0_.instructor_detail_id=instructor2_.id where instructor0_.id=?
+ * Instructor(id=1, firstName=nick, lastName=pete, email=pete@gmail.com, instructorDetial=InstructorDetail [youtubeChannel=http://www.pete.com, hobby=sleeping], courses=[Course [id=12, title=coding], Course [id=13, title=Pinball]])
+ * InstructorDetail [youtubeChannel=http://www.pete.com, hobby=sleeping]
+ * [Course [id=12, title=coding], Course [id=13, title=Pinball]]
+ */
+
+//Lazy
+/*
+ * Hibernate: select instructor0_.id as id1_1_0_, instructor0_.email as email2_1_0_, instructor0_.first_name as first_na3_1_0_, instructor0_.instructor_detail_id as instruct5_1_0_, instructor0_.last_name as last_nam4_1_0_, instructor1_.id as id1_2_1_, instructor1_.hobby as hobby2_2_1_, instructor1_.youtube_channel as youtube_3_2_1_ from instructor instructor0_ left outer join instructor_detail instructor1_ on instructor0_.instructor_detail_id=instructor1_.id where instructor0_.id=?
+ * Instructor(id=1, firstName=nick, lastName=pete, email=pete@gmail.com, instructorDetial=InstructorDetail [youtubeChannel=http://www.pete.com, hobby=sleeping], courses=[Course [id=12, title=coding], Course [id=13, title=Pinball]])
+ * InstructorDetail [youtubeChannel=http://www.pete.com, hobby=sleeping]
+ * Hibernate: select courses0_.instructor_id as instruct3_0_0_, courses0_.id as id1_0_0_, courses0_.id as id1_0_1_, courses0_.instructor_id as instruct3_0_1_, courses0_.title as title2_0_1_ from course courses0_ where courses0_.instructor_id=?
+ * [Course [id=12, title=coding], Course [id=13, title=Pinball]]
+ */
